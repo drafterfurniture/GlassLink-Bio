24837f319e3afe5a73caeb65d180e0a7d4def6a3
Drafter Furniture <reddamaskus@gmail.com>
2026-05-07 10:13:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ fetch("links.json")
 .then(res => res.json())
 .then(data => {
   let el = document.getElementById("links");
+
   data.forEach(l => {
     el.innerHTML += `
       <a href="${l.url}" class="btn">
         <div class="btn-left">
           <i class="${l.icon}"></i>
-          ${l.title}
+          <span>${l.title}</span>
         </div>
         <i class="fas fa-arrow-right"></i>
       </a>
@@ -18,10 +19,10 @@ fetch("links.json")
 
 // GALLERY
 const gallery = [
-  {img:"assets/img/1.webp", link:"#"},
-  {img:"assets/img/2.webp", link:"#"},
-  {img:"assets/img/3.webp", link:"#"},
-  {img:"assets/img/4.webp", link:"#"}
+  {img:"assets/img/1.jpg", link:"#"},
+  {img:"assets/img/2.jpg", link:"#"},
+  {img:"assets/img/3.jpg", link:"#"},
+  {img:"assets/img/4.jpg", link:"#"}
 ];
 
 let g = document.getElementById("gallery");
