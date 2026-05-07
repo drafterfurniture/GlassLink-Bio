5d13e701ba897dcbe0e404c826b4c5a02d29e25d
Drafter Furniture <reddamaskus@gmail.com>
2026-05-07 09:26:58 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// LOAD LINKS
+fetch("links.json")
+.then(res => res.json())
+.then(data => {
+  let el = document.getElementById("links");
+  data.forEach(l => {
+    el.innerHTML += `
+      <a href="${l.url}" class="btn">${l.title}</a>
+    `;
+  });
+});
+
+// GALLERY DATA (bisa dijadiin JSON juga nanti)
+const gallery = [
+  {img:"assets/img/1.jpg", link:"#"},
+  {img:"assets/img/2.jpg", link:"#"},
+  {img:"assets/img/3.jpg", link:"#"},
+  {img:"assets/img/4.jpg", link:"#"}
+];
+
+let g = document.getElementById("gallery");
+
+gallery.forEach(item => {
+  g.innerHTML += `
+    <div class="item">
+      <img src="${item.img}">
+      <a href="${item.link}" target="_blank">
+        <i class="fas fa-arrow-up-right-from-square"></i>
+      </a>
+    </div>
+  `;
+});
